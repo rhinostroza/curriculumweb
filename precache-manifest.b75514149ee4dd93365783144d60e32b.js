c499e9493d136cec55e26b916cc8f44d843ed477
Raphael Hinostroza <raphael.trabajo20@gmail.com>
2020-06-08 17:43:16 -0500

    Updates

---
+++ precache-manifest.b75514149ee4dd93365783144d60e32b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d0e599e93f6fc4f6f1b1bd15d502c4",
+    "url": "/Miweb/index.html"
+  },
+  {
+    "revision": "ddd67cc33807a548db91",
+    "url": "/Miweb/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f53baac682194ecc8fe6",
+    "url": "/Miweb/static/css/main.f26980f0.chunk.css"
+  },
+  {
+    "revision": "ddd67cc33807a548db91",
+    "url": "/Miweb/static/js/2.203c24e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Miweb/static/js/2.203c24e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53baac682194ecc8fe6",
+    "url": "/Miweb/static/js/main.11c07307.chunk.js"
+  },
+  {
+    "revision": "7fa4dee27e2ac02fe644",
+    "url": "/Miweb/static/js/runtime-main.1e260861.js"
+  },
+  {
+    "revision": "b94d18d4f73f284699543647d13e4a0b",
+    "url": "/Miweb/static/media/Gothub2x.b94d18d4.png"
+  },
+  {
+    "revision": "8fc0be629a49019adfc7aacf0b43d0e7",
+    "url": "/Miweb/static/media/Iso.8fc0be62.png"
+  },
+  {
+    "revision": "1ea51b2cad65218707861fc358050180",
+    "url": "/Miweb/static/media/Raphael_Hinostroza2x.1ea51b2c.png"
+  },
+  {
+    "revision": "4487c72b44eeb3415084d40724af2dbf",
+    "url": "/Miweb/static/media/Redux2x.4487c72b.png"
+  },
+  {
+    "revision": "304cc0cd74e93186d45c246a40e30043",
+    "url": "/Miweb/static/media/icomoon.304cc0cd.woff"
+  },
+  {
+    "revision": "482577d6ccd54043241b80764929383e",
+    "url": "/Miweb/static/media/icomoon.482577d6.svg"
+  },
+  {
+    "revision": "a7584677ed2df593f7764181cfa0bd98",
+    "url": "/Miweb/static/media/icomoon.a7584677.ttf"
+  },
+  {
+    "revision": "e43b7b8fb056fb7fe42392258d52abd7",
+    "url": "/Miweb/static/media/icomoon.e43b7b8f.eot"
+  },
+  {
+    "revision": "c15a11d290162b119f309f6d015e251f",
+    "url": "/Miweb/static/media/postmanx2_2.c15a11d2.png"
+  },
+  {
+    "revision": "a2ce3a959c53660159967ab986ad9318",
+    "url": "/Miweb/static/media/react2x.a2ce3a95.png"
+  },
+  {
+    "revision": "786920b0b1f853f1d9ec34accb332d07",
+    "url": "/Miweb/static/media/triangulo2x.786920b0.png"
+  },
+  {
+    "revision": "3f1be60445859086a7a75f09a059ec09",
+    "url": "/Miweb/static/media/webpack@2x.3f1be604.png"
+  }
+]);
